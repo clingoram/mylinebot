ce7a781d5b634ec7e2200a2aa6a522454a8fec46
Mavis <llingchen.27@gmail.com>
2024-04-22 16:05:14 +0800

    持續進行table message error

--- basic_info/apps.py
+++ basic_info/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class InfoConfig(AppConfig):
+class BasicInfoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'info'
+    name = 'basic_info'
