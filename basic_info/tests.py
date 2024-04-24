2718d6201a684bba255581a7a8463262640cb945
Mavis <llingchen.27@gmail.com>
2024-04-24 15:34:37 +0800

    整理bot部分程式碼，並將涵式移至basic_info views內；test case

--- basic_info/tests.py
+++ basic_info/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from datetime import datetime
 
 # Create your tests here.
+from .models import Person,Message
+
+class PersonModelTests(TestCase):
+  def test_perosn_model_exists(self):
+    person = Person.objects.count()
+
+    self.assertEqual(person,0)
+
+  # def test_models_has_string(self):
+  #   person = Person.objects.create(uid="Ukd545gdf8g7df8g",account = "User",created_at = datetime.now())
+  #   self.assertEqual(person,person.uid)
--- basic_info/views.py
+++ basic_info/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from basic_info.models import Person,Message
+from datetime import datetime
+
 
 # Create your views here.
+def create_user(id:str,name:str):
+  '''
+  建立person(user)
+  '''
+  person = Person.objects.create(uid=id, account=name, created_at=datetime.now())
+  person.save()
+
+def insertKeyWord(user_id:str,keyword:str):
+  '''
+  儲存使用者在聊天室搜尋(關鍵字)
+  '''
+  if Person.objects.filter(uid=user_id).exists():
+    # 將user message存到message
+    person = Person.objects.get(uid=user_id)
+    person.updated_at = datetime.now()
+    person.save()
+
+    # person.uid
+    msg = Message.objects.create(uid = person, contentKeyWord = keyword,created_at = datetime.now())
+    msg.save()
