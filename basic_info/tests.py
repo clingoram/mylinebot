e2692ae881124af1e4252698808723b112e26dd5
Mavis <llingchen.27@gmail.com>
2024-04-25 15:35:23 +0800

    test case

--- basic_info/tests.py
+++ basic_info/tests.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
 from datetime import datetime
-
-# Create your tests here.
 from .models import Person,Message
 
+# Create your tests here.
 class PersonModelTests(TestCase):
   def test_perosn_model_exists(self):
     person = Person.objects.count()
@@ -13,3 +12,8 @@ class PersonModelTests(TestCase):
   # def test_models_has_string(self):
   #   person = Person.objects.create(uid="Ukd545gdf8g7df8g",account = "User",created_at = datetime.now())
   #   self.assertEqual(person,person.uid)
+
+
+class MessageModelTests(TestCase):
+  def test_message_model_exists(self):
+    msg = Message()
